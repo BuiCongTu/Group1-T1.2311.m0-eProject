7e32875ffe1466a4997dcf0fc7d6c86d3d4838e7
BuiCongTu <congtu7677@gmail.com>
2024-03-28 20:25:02 +0700

    28-3 20h

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-// Gọi hàm để khi nhấn cancel
+// Gọi hàm khi nhấn cancel
 
 function cancel() {
   window.location.href = "./index.html";
